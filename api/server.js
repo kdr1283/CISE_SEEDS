975fc20452785dfd9d78225fd329cff37ec99a79
Rafael <ramos.rafael42@gmail.com>
2021-05-07 18:57:52 +1200

    renamed app.js to server.js

--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ if(process.env.NODE_ENV === "production") {
         res.send("Api running")
     })
 }
-
+ 
 const port = process.env.PORT
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
