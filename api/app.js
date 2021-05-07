ec9112dc9e30285e080e05812320ea3f9635b2f3
Rafael <68944880+kdr1283@users.noreply.github.com>
2021-05-07 12:53:28 +1200

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,3 +1,5 @@
+require("dotenv").config({ path: "./config.env" })
+const path = require('path')
 const express = require("express")
 const connectDB = require("./config/db")
 
@@ -8,5 +10,9 @@ connectDB()
 app.get('/', (req, res) => res.send("Hello world! MongoDB connected"))
 
 const port = process.env.PORT || 8082
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/client/build')))
+}
 
 app.listen(port, () => console.log("Server running on port ${port}"))
