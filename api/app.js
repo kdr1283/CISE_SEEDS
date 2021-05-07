59fc34ff0880844fa920e7fe739c3b6f32e28933
Rafael <68944880+kdr1283@users.noreply.github.com>
2021-05-07 13:30:40 +1200

    final deployment

--- api/app.js
+++ api/app.js
@@ -13,6 +13,14 @@ const port = process.env.PORT || 8082
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '/client/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    })
+} else {
+    app.get("/", (req, res) => {
+        res.send("Api running")
+    })
 }
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
