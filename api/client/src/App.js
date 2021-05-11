6eb4a6dffd21eecf259234d3e7523b260312cf1d
Rafael <ramos.rafael42@gmail.com>
2021-05-11 22:29:38 +1200

    ArticleCard.js and ShowArticleList.js

--- api/client/src/App.js
+++ api/client/src/App.js
@@ -1,26 +1,20 @@
-/* eslint-disable react/react-in-jsx-scope */
-import logo from "./logo.svg"
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+import ShowBookList from "./components/ShowArticleList"
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={ShowBookList} />
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App
---
+++ api/client/src/components/ShowArticleList.js
@@ -0,0 +1,74 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable no-console */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-unused-vars */
+import React, { Component } from "react"
+import "../App.css"
+import axios from "axios"
+import { Link } from "react-router-dom"
+import ArticleCard from "./ArticleCard"
+
+class ShowArticleList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      articles: [],
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:8082/api/articles")
+      .then((res) => {
+        this.setState({
+          articles: res.data,
+        })
+      })
+      .catch((err) => {
+        console.log("Error from ShowArticleList")
+      })
+  }
+
+  render() {
+    const { articles } = this.state
+    console.log(`PrintArticle: ${articles}`)
+    let articleList
+
+    if (!articles) {
+      articleList = "there is no article record!"
+    } else {
+      articleList = articles.map((article, k) => (
+        <ArticleCard article={article} key={k} />
+      ))
+    }
+
+    return (
+      <div className="ShowArticleList">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <br />
+              <h2 className="display-4 text-center">Articles List</h2>
+            </div>
+
+            <div className="col-md-11">
+              <Link
+                to="/create-article"
+                className="btn btn-outline-warning float-right"
+              >
+                + Add New Article
+              </Link>
+              <br />
+              <br />
+              <hr />
+            </div>
+          </div>
+
+          <div className="list">{articleList}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ShowArticleList
