3b4097c03131929d0866b949252c7f9a03ed4f97
Rafael <ramos.rafael42@gmail.com>
2021-05-11 22:17:12 +1200

    Update articles.js
    get all articles

--- api/routes/api/articles.js
+++ api/routes/api/articles.js
@@ -1,4 +1,12 @@
 const express = require("express")
 const router = express.Router()
 
-const Book = require("../../models/Article")
+const Article = require("../../models/Article")
+
+router.get("/", (req, res) => {
+    Article.find()
+      .then(books => res.json(books))
+      .catch(err => res.status(404).json({ nobooksfound: 'No Books found' }))
+  })
+
+  module.exports = router;
